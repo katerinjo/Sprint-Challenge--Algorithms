134231c5be16cbbfbbdfa85ca485bcd8550f17d7
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-12-13 12:37:28 -0600

    successful run

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -92,12 +92,78 @@ class SortingRobot:
         """
         return self._light == "ON"
 
+    def show_state(self):
+        print('-------')
+        for i in range(len(self._list)):
+            print(self._list[i], end='')
+            if i == self._position:
+                if self._light == 'ON':
+                    print(' !', self._item, '!')
+                else:
+                    print(' (', self._item, ')')
+            else:
+                print()
+
+
     def sort(self):
         """
         Sort the robot's list.
         """
         # Fill this out
-        pass
+        '''
+go right until you find the next unsorted
+on-(hold) + None + right-space -> swap, move_right, light_off = off-(empty)
+on-(hold) + None + right-border -> swap, END
+on-(hold) + l>h -> swap, move_right
+
+go left until you find its place
+off-(empty) + None + left-space -> swap, move_left = off-(hold)
+off-(empty) + None + left-border -> move_right = on-(hold)
+off-(hold) + l>h -> move_left = off-(hold)
+off-(hold) + l>h + left-border -> swap, light_on, move_right = on-(hold)
+off-(hold) + l<=h -> light_on, move_right = on-(hold)
+        '''
+        self.show_state()
+        input('')
+        if self.light_is_on():
+            if self.compare_item() is None:
+                if self.can_move_right():
+                    print('--> reached border, go left')
+                    self.swap_item()
+                    self.move_right()
+                    self.set_light_off()
+                else:
+                    print('--> final insertion')
+                    self.swap_item()
+                    return
+            else:
+                print('--> bubble up')
+                self.swap_item()
+                self.move_right()
+        else:
+            if self.compare_item() is None:
+                if self.can_move_left():
+                    print('--> create the partition')
+                    self.swap_item()
+                    self.move_left()
+                else:
+                    print('--> left border')
+                    self.move_right()
+            else:
+                if self.compare_item() == -1:
+                    if self.can_move_left():
+                        print('--> not right place yet')
+                        self.move_left()
+                    else:
+                        print('--> none left to be smaller, start shifting')
+                        self.swap_item()
+                        self.set_light_on()
+                        self.move_right()
+                else:
+                    print('--> correct place! start bubbling')
+                    self.set_light_on()
+                    self.move_right()
+        self.sort()
 
 
 if __name__ == "__main__":
@@ -106,7 +172,7 @@ if __name__ == "__main__":
 
     l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
 
-    robot = SortingRobot(l)
+    robot = SortingRobot([28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 60, 54, 5, 0])
 
     robot.sort()
     print(robot._list)
